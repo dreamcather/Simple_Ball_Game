ba0f339999d61b534be77ba6d2f131850cca0da6
evginiy <dreamcather1997@yandex.ru>
2018-09-07 12:56:08 +0300

    Add Abstract Class Ball

--- src/sample/Ball.java
+++ src/sample/Ball.java
@@ -2,22 +2,27 @@ package sample;
 
 import javafx.scene.shape.Circle;
 
-public class Ball {
-    double x; //x coordinate of vector motion
-    double y; //y coordinate of vector motion
-    double speed; // distance ball for frame
-    Circle model; // model for visualisation
-    GameState game; // game state management
+public abstract class Ball {
+    private double xCoefficient;
+    private double yCoefficient;
+    private double speedOfmotion;
+    private Circle gameModel;
+    private GameState gameStatus;
     Ball(double _x, double _y, double _speed, GameState _game){
-        x=_x;
-        y=_y;
-        speed=_speed;
-        model = new Circle();
-        game = _game;
-        game.AddBall(this);
+        xCoefficient=_x;
+        yCoefficient=_y;
+        speedOfmotion=_speed;
+        gameStatus = _game;
     }
-    void Move(){
-        model.setCenterX(model.getCenterX()+speed*x);
-        model.setCenterY(model.getCenterY()+speed*y);
+    private void Move(){
     }
+
+    public abstract Boolean CollisionWithWall();
+
+    public abstract Boolean CollisionWithHero();
+
+    public abstract Boolean CollisionWithPoint();
+
+    public abstract Boolean CollisionWithEnemy();
+
 }
