7a65fe09845ec0f98aaa4dae1f42f1073dcd3f33
evginiy <dreamcather1997@yandex.ru>
2018-09-28 13:59:32 +0300

    work Station

---
+++ src/test/java/sample/CameraTest.java
@@ -0,0 +1,36 @@
+package sample;
+
+import gameObject.Wall;
+import geometry.Point;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import visual.Camera;
+
+public class CameraTest {
+//    @Test
+//    public void isVisiblePointTest1(){
+//        Camera camera = new Camera(new Point(250,250));
+//        Point point = new Point(300,300);
+//
+//        boolean actual = camera.isVisible(point);
+//        Assert.assertTrue(actual);
+//    }
+//
+//    @Test
+//    public void isVisiblePointTest2(){
+//        Camera camera = new Camera(new Point(250,250));
+//        Point point = new Point(600,600);
+//
+//        boolean actual = camera.isVisible(point);
+//        Assert.assertFalse(actual);
+//    }
+//
+//    @Test
+//    public void isVisibleWallTest1(){
+//        Camera camera = new Camera(new Point(250,250));
+//        Wall wall = new Wall(new Point(250,250),new Point(250,750));
+//        boolean actual = camera.isVisible(wall);
+//
+//        Assert.assertTrue(actual);
+//    }
+}
