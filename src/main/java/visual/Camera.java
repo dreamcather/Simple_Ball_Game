67b62e0ff8b73eca0920c3eea1c5e29bad9b93d4
Dvorjanchikov_Evgeniy <dreamcather1997@yandex.ru>
2018-09-29 18:14:08 +0300

    add camera class constructor and transformPoint method

--- src/main/java/visual/Camera.java
+++ src/main/java/visual/Camera.java
@@ -1,13 +1,27 @@
 package visual;
 
+import gameObject.Wall;
 import geometry.Point;
 
 public class Camera {
     Point position;
-    int weight=250;
+    int weight = 250;
+    Wall upHorizontal;
+    Wall downHorzontal;
+    Wall leftVertical;
+    Wall rightVertical;
 
     public Camera(Point position) {
         this.position = position;
+        upHorizontal = new Wall(new Point(0, 2 * weight), new Point(0, 0));
+        downHorzontal = new Wall(new Point(0, 2 * weight), new Point(2 * weight, 2 * weight));
+        leftVertical = new Wall(new Point(0, 0), new Point(0, 2 * weight));
+        rightVertical = new Wall(new Point(2 * weight, 2 * weight), new Point(2 * weight, 0));
+    }
+
+    public Point transformPoint(Point point) {
+        return new Point(point.getX() - position.getX() + weight,
+                point.getY() - position.getY() + weight);
     }
 
     public Point getPosition() {
