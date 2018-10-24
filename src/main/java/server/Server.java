a571ed54d943db407d266e3dcb2313e38ae63ae0
evginiy <dreamcather1997@yandex.ru>
2018-10-24 17:02:56 +0300

    correct

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -3,11 +3,15 @@ package server;
 import java.io.IOException;
 import java.rmi.AlreadyBoundException;
 import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 public class Server {
     public static void main(final String[] args) throws IOException, AlreadyBoundException {
         BridgeClass bridge = new BridgeClass();
+        Registry localReg = LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
         Naming.rebind("rmi://192.168.1.111/key",bridge);
+
         System.out.println("Server work");
     }
 }
